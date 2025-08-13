a4d7ac9167b18b29f270d04273b9c600dca674d5
Parkyounh <dkrfud9@naver.com>
2025-08-13 16:47:24 +0900

    stream

---
+++ src/main/java/stream/StreamEx.java
@@ -0,0 +1,29 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class StreamEx {
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("김","박","정","최");
+		for(String str :list) {
+			System.out.print(str);
+		}
+		list.stream().forEach(n -> System.out.print(n));
+		
+		
+		int sum = IntStream.rangeClosed(1, 10).sum();
+		System.out.println(sum);
+		
+//		IntStream.rangeClosed(1, 10).forEach(System.out::println);
+		
+		List<String> items = Arrays.asList("apple","banan","orange","apple");
+		long count = items.stream().filter(item -> item.equals("apple")).count();
+		System.out.println(count);
+		
+		List<Integer> nums = Arrays.asList(5,1,9,3);
+		int max = nums.stream().max((a,b) -> a-b).get();
+		System.out.println(max);
+	}
+}
